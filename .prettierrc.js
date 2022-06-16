972287bae07bb02a16d1cf69ec82459dc43d8b33
new-wang <1569566735@qq.com>
2022-06-16 12:00:54 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // printWidth: 80,
+    // tabWidth: 2,
+    // useTabs: false,
+    semi: false, // 末尾逗号，default: true
+    singleQuote: true, // 单引号 default: false
+    // quoteProps: 'as-needed',
+    // jsSingleQuote: false,
+    trailingComma: "none", // 末尾分号 default: es5     all
+    // bracketSpacing: true,
+    // bracketSameLine: false,
+    // jsxBracketSameLine: false,
+    arrowParents: "avoid", // default: always
+    // insertPragma: false,
+    // requirePragma: false,
+    proseWrap: "never",
+    // htmlWhitespaceSensitivity: 'CSS',
+    // vueIndentScriptAndStyle: false, // .vue 缩进
+    endOfLine: "auto", // default lf
+};
