737dd4cf4914b8b6a3ba56104251d1bd618f61fe
new-wang <1569566735@qq.com>
2022-06-17 16:03:26 +0800

    *：添加别名 *：添加vitepress对tsx的支持插件

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-import path from "path";
-
 export default defineConfig({
-  plugins: [vueJsx({})],
-  resolve:{
-    alias:{
-      '@comp':path.resolve(__dirname, 'src/components'),
-    }
-  }
+  plugins: [vueJsx({})]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),vueJsx({})]
+  plugins: [vue(),vueJsx({})],
+  resolve:{
+    alias:{
+      '@docs':path.resolve(__dirname, 'docs'),
+      '@style':path.resolve(__dirname, 'src/style'),
+      '@comp':path.resolve(__dirname, 'src/components'),
+    }
+  }
 })
