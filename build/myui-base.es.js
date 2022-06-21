310555c2c0e79a6194a7722ced7998fae94de11f
new-wang <1569566735@qq.com>
2022-06-21 17:23:45 +0800

    *：全量打包输出的文件

---
+++ build/myui-base.es.js
@@ -0,0 +1,64 @@
+import { defineComponent, toRefs, createVNode } from "vue";
+const buttonProps = {
+  type: {
+    type: String,
+    default: "secondary"
+  },
+  size: {
+    type: String,
+    default: "medium"
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  block: {
+    type: Boolean,
+    default: false
+  },
+  tag: {
+    type: String,
+    default: "button"
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  },
+  onClick: {
+    type: [Function, Array]
+  }
+};
+var Button = defineComponent({
+  name: "SButton",
+  props: buttonProps,
+  setup(props, {
+    slots
+  }) {
+    const {
+      type,
+      size,
+      disabled,
+      block
+    } = toRefs(props);
+    return () => {
+      const defaultSlots = slots.default ? slots.default() : "\u6309\u94AE";
+      const blockCls = block.value ? "s-btn--block" : "";
+      return createVNode("button", {
+        "disabled": disabled.value,
+        "class": `s-btn s-btn--${type.value} s-btn--${size.value} ${blockCls}`
+      }, [defaultSlots]);
+    };
+  }
+});
+var ButtonPlugin = {
+  install(app) {
+    app.component(Button.name, Button);
+  }
+};
+const installs = [ButtonPlugin];
+var entry = {
+  install(app) {
+    installs.forEach((p) => app.use(p));
+  }
+};
+export { Button, entry as default };
